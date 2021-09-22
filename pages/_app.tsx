bbb754cb1f770d5e0bb9bfa759c174069e695988
Nazar Velychko <nazar.v@radency.com>
2021-09-23 01:33:28 +0300

    added header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import 'styles/base/reset.scss';
-
-import type { AppProps } from 'next/app';
 import React from 'react';
+import type { AppProps } from 'next/app';
 import EstatesProvider from 'contexts/real-estates/components';
+import Header from 'layout/header/header';
+import 'styles/base/reset.scss';
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   return (
     <EstatesProvider>
+      <Header />
       <Component {...pageProps} />
     </EstatesProvider>
   );
