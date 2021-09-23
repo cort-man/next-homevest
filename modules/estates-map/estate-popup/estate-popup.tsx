85deff44ca8c9aca4c0bc8b798d5f8a581aa5330
Nazar Velychko <nazar.v@radency.com>
2021-09-23 20:51:42 +0300

    created popup card for map

--- modules/estates-map/estate-popup/estate-popup.tsx
+++ modules/estates-map/estate-popup/estate-popup.tsx
@@ -3,7 +3,14 @@ import { IRealEstate } from 'common/interfaces';
 import { Popup } from 'react-map-gl';
 import classes from 'styles/modules/estates-map/estates-popup.module.scss';
 import { XIcon } from '@heroicons/react/outline';
-import { HomeIcon } from '@heroicons/react/solid';
+import {
+  HomeIcon,
+  StarIcon,
+  UserIcon,
+  MusicNoteIcon,
+  LightBulbIcon,
+  BeakerIcon,
+} from '@heroicons/react/solid';
 
 interface IEstatePopupProps {
   setSelectedProperty: (property: IRealEstate) => void;
@@ -26,14 +33,66 @@ const EstatePopup: React.FC<IEstatePopupProps> = ({ selectedProperty }) => (
         <div className={classes.top}>
           <div className={classes.top__button}>
             <HomeIcon className={classes.home_icon} />
-            <span>Entire Home</span>
+            <span className={classes.home_text}>Entire Home</span>
           </div>
           <div className={classes.top__button_close}>
             <XIcon className={classes.icon} />
           </div>
         </div>
+        <div className={classes.bottom}>
+          <div className={classes.icon_container}>
+            <MusicNoteIcon className={classes.icon} />
+            <span>2</span>
+          </div>
+          <div className={classes.icon_container}>
+            <LightBulbIcon className={classes.icon} />
+            <span>2.5</span>
+          </div>
+          <div className={classes.icon_container}>
+            <UserIcon className={classes.icon} />
+            <span>4</span>
+          </div>
+          <div className={classes.icon_container}>
+            <StarIcon className={classes.icon} />
+            <span>47.3</span>
+          </div>
+        </div>
       </div>
-      <div>asd</div>
+      <div className={classes.content}>
+        <div className={classes.content__info}>
+          <div className={classes.title}>
+            <div>Best Loft in Denver, Lodo</div>
+            <BeakerIcon className={classes.icon} />
+          </div>
+          <div className={classes.description}>
+            <div>
+              Loft in
+              <span className={classes.description__selected}> LoDo</span>
+            </div>
+          </div>
+        </div>
+        <div className={classes.content__stats}>
+          <div className={classes.header}>Last 12 Month</div>
+          <div className={classes.container}>
+            <div className={classes.card}>
+              <div className={classes.card__value}>61</div>
+              <div className={classes.card__name}>Days Available</div>
+            </div>
+            <div className={classes.card}>
+              <div className={classes.card__value}>$764</div>
+              <div className={classes.card__name}>Avg. Daily Rate</div>
+            </div>
+            <div className={classes.card}>
+              <div className={classes.card__value}>59%</div>
+              <div className={classes.card__name}>Occupancy</div>
+            </div>
+            <div className={classes.card}>
+              <div className={classes.card__value}>$27.5k</div>
+              <div className={classes.card__name}>Revenue</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </Popup>
 );
