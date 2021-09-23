02b1ac8bb5b819f569608139fee7c8eaab14ebb2
Nazar Velychko <nazar.v@radency.com>
2021-09-23 09:38:59 +0300

    added map

--- common/enums/env.ts
+++ common/enums/env.ts
@@ -1,13 +1,16 @@
 const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
 const API_PATH = process.env.NEXT_PUBLIC_API_PATH;
 const API_ORIGIN_URL = process.env.NEXT_PUBLIC_API_ORIGIN_URL;
+const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 if (!API_HOST) throw new Error('provide API_HOST');
 if (!API_PATH) throw new Error('provide API_PATH');
-if (!API_PATH) throw new Error('provide API_PATH');
+if (!API_ORIGIN_URL) throw new Error('provide API_PATH');
+if (!MAPBOX_ACCESS_TOKEN) throw new Error('provide MAPBOX_ACCESS_TOKEN');
 
 export const ENV = {
   API_ORIGIN_URL,
   API_HOST,
   API_PATH,
+  MAPBOX_ACCESS_TOKEN,
 };
---
+++ pages/real-estates-map/real-estates-map.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import EstatesMap from 'modules/estates-map/EstatesMap';
+import { useEstates } from 'contexts/real-estates/hooks';
+import { MapViewport } from 'common/interfaces';
+
+const defaultMapValue: MapViewport = {
+  latitude: 50.4501,
+  longitude: 30.5234,
+  zoom: 10,
+};
+
+const RealEstatesMap: React.FC = () => {
+  const [mapViewport, setMapViewport] = useState(defaultMapValue);
+
+  const estatesState = useEstates();
+
+  return (
+    <div>
+      <EstatesMap
+        estates={estatesState.data}
+        mapViewport={mapViewport}
+        setViewport={setMapViewport}
+      />
+    </div>
+  );
+};
+
+export { RealEstatesMap };
