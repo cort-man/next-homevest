7fa1738635e94ed9df99ccaf90940a44d92eb8e6
Nazar Velychko <nazar.v@radency.com>
2021-09-28 08:37:16 +0300

    created hook for requests

---
+++ common/hooks/useRequest.ts
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from 'react';
+
+interface IUseRequestReturn<Args, ReturnData> {
+  loading: boolean;
+  error: Error | undefined;
+  data: ReturnData | undefined;
+  setRequest: (args: Args | null) => void;
+}
+
+const useRequest = <Args, ReturnData>(
+  asyncFunc: (args: Args) => Promise<ReturnData>
+): IUseRequestReturn<Args, ReturnData> => {
+  const [loading, setLoading] = useState(false);
+  const [request, setRequest] = useState<Args | null>(null);
+  const [data, setData] = useState<ReturnData | undefined>();
+  const [error, setError] = useState<Error>();
+
+  const makeRequest = useCallback(async () => {
+    if (request === null) return;
+
+    setLoading(true);
+
+    try {
+      const data = await asyncFunc(request);
+      setData(data);
+    } catch (error) {
+      setError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  }, [request, asyncFunc]);
+
+  useEffect(() => {
+    makeRequest();
+  }, [makeRequest]);
+
+  return { loading, error, data, setRequest };
+};
+
+export { useRequest };
